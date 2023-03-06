96a938220c36cdef1e80a60109649e3490f0f46a
Emill <lemito66@gmail.com>
2023-03-06 14:33:56 -0500

    añadiendo modelo

---
+++ src/views/tr/pedidos/Models/obtenerDatos.js
@@ -0,0 +1,111 @@
+import m from "mithril";
+
+const terapiaRespiratoriaController = {
+  lista: [],
+  error: "",
+  listaDeFrecuenciaCardiaca: [],
+  listaDeFrecuenciaRespiratoria: [],
+  listaDePeso: [],
+
+  cargarPrescripcion: function (numeroDeAtendimiento) {
+    m.request({
+      method: "GET",
+      url: `https://api.hospitalmetropolitano.org/t/v1/tr/formularios/sv?PARAM=PRESC&&CD_ATENDIMENTO=${numeroDeAtendimiento}`,
+      body: {},
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        Accept: "application/json",
+        Authorization: localStorage.accessToken,
+      },
+    })
+      .then(function (resultado) {
+        if (resultado.status && resultado.data.length > 0) {
+          terapiaRespiratoriaController.lista = resultado;
+        } else {
+          terapiaRespiratoriaController.error = resultado.error;
+          alert(terapiaRespiratoriaController.error);
+        }
+      })
+      .catch(function (error) {
+        terapiaRespiratoriaController.error = error;
+        alert(terapiaRespiratoriaController.error);
+      });
+  },
+
+  cargarFrecuenciaCardiaca: function (numeroDeAtendimiento) {
+    m.request({
+      method: "GET",
+      url: `https://api.hospitalmetropolitano.org/t/v1/tr/formularios/sv?PARAM=FRECUENCIA_CARDIACA&CD_ATENDIMENTO=${numeroDeAtendimiento}`,
+      body: {},
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        Accept: "application/json",
+        Authorization: localStorage.accessToken,
+      },
+    })
+      .then(function (resultado) {
+        if (resultado.status) {
+          terapiaRespiratoriaController.listaDeFrecuenciaCardiaca = resultado;
+        } else {
+          terapiaRespiratoriaController.error = resultado.error;
+          alert(terapiaRespiratoriaController.error);
+        }
+      })
+      .catch(function (error) {
+        terapiaRespiratoriaController.error = error;
+        alert(terapiaRespiratoriaController.error);
+      });
+  },
+  cargarFrecuenciaRespiratoria: function (numeroDeAtendimiento) {
+    m.request({
+      method: "GET",
+      url: `https://api.hospitalmetropolitano.org/t/v1/tr/formularios/sv?PARAM=FRECUENCIA_RESPIRATORIA&CD_ATENDIMENTO=${numeroDeAtendimiento}`,
+      body: {},
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        Accept: "application/json",
+        Authorization: localStorage.accessToken,
+      },
+    })
+      .then(function (resultado) {
+        if (resultado.status) {
+          terapiaRespiratoriaController.cargarFrecuenciaRespiratoria =
+            resultado;
+        } else {
+          terapiaRespiratoriaController.error = resultado.error;
+          alert(terapiaRespiratoriaController.error);
+        }
+      })
+      .catch(function (error) {
+        terapiaRespiratoriaController.error = error;
+        alert(terapiaRespiratoriaController.error);
+      });
+  },
+
+  cargarPeso: function (numeroDeAtendimiento) {
+    m.request({
+      method: "GET",
+      url: `https://api.hospitalmetropolitano.org/t/v1/tr/formularios/sv?PARAM=PESO&CD_ATENDIMENTO=${numeroDeAtendimiento}`,
+      body: {},
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        Accept: "application/json",
+        Authorization: localStorage.accessToken,
+      },
+    })
+      .then(function (resultado) {
+        if (resultado.status) {
+          terapiaRespiratoriaController.listaDePeso = resultado;
+        } else {
+          terapiaRespiratoriaController.error = resultado.error;
+          alert(terapiaRespiratoriaController.error);
+        }
+      })
+      .catch(function (error) {
+        terapiaRespiratoriaController.error = error;
+        alert(terapiaRespiratoriaController.error);
+      });
+  },
+};
+
+export default terapiaRespiratoriaController;

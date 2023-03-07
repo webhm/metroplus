88def196b94cc647a67265f7b8a7f1a7c0243737
Emill <lemito66@gmail.com>
2023-03-07 12:50:58 -0500

    habilitar y deshabilitar campos

--- src/views/tr/pedidos/formularioDeRegistro.js
+++ src/views/tr/pedidos/formularioDeRegistro.js
@@ -1,20 +1,22 @@
 import m from "mithril";
 import terapiaRespiratoriaController from "./Models/obtenerDatos";
 import Pedido from "./pedido";
-import Encrypt from '../../../models/encrypt';
+import Encrypt from "../../../models/encrypt";
 
 const FormularioDeRegistro = {
   oninit: () => {
-    
     terapiaRespiratoriaController.cargarFrecuenciaCardiaca(10090);
     terapiaRespiratoriaController.cargarFrecuenciaRespiratoria(10090);
     terapiaRespiratoriaController.cargarPeso(10090);
     terapiaRespiratoriaController.cargarEscalaDelDolor(10090);
 
     terapiaRespiratoriaController.cargarPrescripcion(2780); //Aqui poner el numero de atendimiento
+    //terapiaRespiratoriaController.cargarPrescripcion(10090); //Aqui poner el numero de atendimiento
     terapiaRespiratoriaController.cargarFechaActual();
     terapiaRespiratoriaController.cargarHoraActual();
-    
+    console.log(`Emill 1  ${terapiaRespiratoriaController.habilitarCampos}`);
+    console.log(`Emill 2  ${!terapiaRespiratoriaController.habilitarCampos}`);
+
     FormularioDeRegistro.usuarioConectado = Encrypt.getDataUser(); // Obtener el nombre de usuario
   },
   usuarioConectado: [],
@@ -29,7 +31,9 @@ const FormularioDeRegistro = {
             id: "inputFrecuenciaCardiaca",
             placeholder: "Frecuencia Cardiaca",
             readonly: "readonly",
-            value: terapiaRespiratoriaController.listaDeFrecuenciaCardiaca.data[0].VALUE,
+            value:
+              terapiaRespiratoriaController.listaDeFrecuenciaCardiaca.data[0]
+                .VALUE,
           }),
         ]),
         m("div", { class: "form-group col-md-4" }, [
@@ -44,7 +48,9 @@ const FormularioDeRegistro = {
             id: "inputFrecuenciaRespiratoria",
             placeholder: "Frecuencia Respiratoria",
             readonly: "readonly",
-            value: terapiaRespiratoriaController.listaDeFrecuenciaRespiratoria.data[0].VALUE,
+            value:
+              terapiaRespiratoriaController.listaDeFrecuenciaRespiratoria
+                .data[0].VALUE,
           }),
         ]),
         m("div", { class: "form-group col-md-4" }, [
@@ -68,7 +74,8 @@ const FormularioDeRegistro = {
             id: "inputEscalaDolor",
             placeholder: "Escala Dolor",
             readonly: "readonly",
-            value: terapiaRespiratoriaController.listaEscalaDelDolor.data[0].VALUE,
+            value:
+              terapiaRespiratoriaController.listaEscalaDelDolor.data[0].VALUE,
           }),
         ]),
         m("div", { class: "form-group col-md-4" }, [
@@ -103,7 +110,9 @@ const FormularioDeRegistro = {
             return m(
               "option",
               { value: `${prescripcion.CODIGO} ${prescripcion.FECHA}` },
-              prescripcion.CODIGO, " ", prescripcion.FECHA
+              prescripcion.CODIGO,
+              " ",
+              prescripcion.FECHA
             );
           })
         ),
@@ -141,23 +150,30 @@ const FormularioDeRegistro = {
       ]),
       m("div", { class: "form-group" }, [
         m("label", { for: "inputFrecuenciaAlDia" }, "Frecuencia al día"),
-        m("select", { class: "custom-select" }, [
-          m("option", "Seleccione..."),
-          m("option", { value: "1" }, "En este momento"),
-          m("option", { value: "2" }, "Una vez"),
-          m("option", { value: "3" }, "Dos veces"),
-          m("option", { value: "4" }, "Tres veces"),
-          m("option", { value: "5" }, "Cuatro veces"),
-          m("option", { value: "6" }, "Cada hora"),
-          m("option", { value: "7" }, "Cada dos horas"),
-          m("option", { value: "8" }, "Cada tres horas"),
-          m("option", { value: "9" }, "Cada cuatro horas"),
-          m("option", { value: "10" }, "Cada sesis horas"),
-          m("option", { value: "11" }, "Cada ocho horas"),
-          m("option", { value: "12" }, "Cada doce horas"),
-          m("option", { value: "13" }, "Set"),
-          m("option", { value: "14" }, "Respetar horas de sueño"),
-        ]),
+        m(
+          "select",
+          {
+            class: "custom-select",
+            disabled: terapiaRespiratoriaController.habilitarCampos,
+          },
+          [
+            m("option", "Seleccione..."),
+            m("option", { value: "1" }, "En este momento"),
+            m("option", { value: "2" }, "Una vez"),
+            m("option", { value: "3" }, "Dos veces"),
+            m("option", { value: "4" }, "Tres veces"),
+            m("option", { value: "5" }, "Cuatro veces"),
+            m("option", { value: "6" }, "Cada hora"),
+            m("option", { value: "7" }, "Cada dos horas"),
+            m("option", { value: "8" }, "Cada tres horas"),
+            m("option", { value: "9" }, "Cada cuatro horas"),
+            m("option", { value: "10" }, "Cada sesis horas"),
+            m("option", { value: "11" }, "Cada ocho horas"),
+            m("option", { value: "12" }, "Cada doce horas"),
+            m("option", { value: "13" }, "Set"),
+            m("option", { value: "14" }, "Respetar horas de sueño"),
+          ]
+        ),
       ]),
       m(
         "div",
@@ -169,25 +185,40 @@ const FormularioDeRegistro = {
             "Terapia Aerosol Medicinas"
           ),
           m("div", { class: "input-group" }, [
-            m("select", { class: "custom-select" }, [
-              m("option", "Seleccione..."),
-              m("option", { value: "1" }, "Nebulización"),
-              m("option", { value: "2" }, "Ultrasonido"),
-              m("option", { value: "3" }, "Inhaladores dosis medida"),
-            ]),
-            m("select", { class: "custom-select" }, [
-              m("option", "Seleccione..."),
-              m("option", { value: "1" }, "Salbumatol"),
-              m("option", { value: "2" }, "Bromuro de ipratropio"),
-              m("option", { value: "3" }, "N acetilcisteina"),
-              m("option", { value: "3" }, "Clorhidrato de ambroxol"),
-              m("option", { value: "5" }, "Hipersal (3,5%)"),
-              m("option", { value: "6" }, "Hipersal (7%)"),
-              m("option", { value: "7" }, "Dexametasona"),
-              m("option", { value: "8" }, "Solución salina (0,9%)"),
-              m("option", { value: "9" }, "Adrenalina Racénica"),
-              m("option", { value: "10" }, "Otros"),
-            ]),
+            m(
+              "select",
+              {
+                class: "custom-select",
+                disabled: terapiaRespiratoriaController.habilitarCampos,
+              },
+              [
+                m("option", "Seleccione..."),
+                m("option", { value: "1" }, "Nebulización"),
+                m("option", { value: "2" }, "Ultrasonido"),
+                m("option", { value: "3" }, "Inhaladores dosis medida"),
+              ]
+            ),
+            m(
+              "select",
+              {
+                class: "custom-select",
+                disabled: terapiaRespiratoriaController.habilitarCampos,
+                //disabled: true,
+              },
+              [
+                m("option", "Seleccione..."),
+                m("option", { value: "1" }, "Salbumatol"),
+                m("option", { value: "2" }, "Bromuro de ipratropio"),
+                m("option", { value: "3" }, "N acetilcisteina"),
+                m("option", { value: "3" }, "Clorhidrato de ambroxol"),
+                m("option", { value: "5" }, "Hipersal (3,5%)"),
+                m("option", { value: "6" }, "Hipersal (7%)"),
+                m("option", { value: "7" }, "Dexametasona"),
+                m("option", { value: "8" }, "Solución salina (0,9%)"),
+                m("option", { value: "9" }, "Adrenalina Racénica"),
+                m("option", { value: "10" }, "Otros"),
+              ]
+            ),
           ]),
         ])
       ),
@@ -197,15 +228,22 @@ const FormularioDeRegistro = {
           { for: "inputHigieneBroncoPulmonar" },
           "Higiene Bronco Pulmonar"
         ),
-        m("select", { class: "custom-select" }, [
-          m("option", "Seleccione..."),
-          m("option", { value: "1" }, "Drenaje postural"),
-          m("option", { value: "2" }, "Percusiones"),
-          m("option", { value: "3" }, "Vibraciones"),
-          m("option", { value: "4" }, "Tos efectiva"),
-          m("option", { value: "5" }, "Asistente de tos"),
-          m("option", { value: "6" }, "Chaleco Vibroprecutor"),
-        ]),
+        m(
+          "select",
+          {
+            class: "custom-select",
+            disabled: terapiaRespiratoriaController.habilitarCampos,
+          },
+          [
+            m("option", "Seleccione..."),
+            m("option", { value: "1" }, "Drenaje postural"),
+            m("option", { value: "2" }, "Percusiones"),
+            m("option", { value: "3" }, "Vibraciones"),
+            m("option", { value: "4" }, "Tos efectiva"),
+            m("option", { value: "5" }, "Asistente de tos"),
+            m("option", { value: "6" }, "Chaleco Vibroprecutor"),
+          ]
+        ),
       ]),
       m(
         "div",
@@ -229,6 +267,7 @@ const FormularioDeRegistro = {
                     inputTerapiaExpansiva2.value = "";
                   }
                 },
+                disabled: terapiaRespiratoriaController.habilitarCampos,
               },
               [
                 m("option", "Seleccione..."),
@@ -269,17 +308,24 @@ const FormularioDeRegistro = {
         m("div", { class: "form-group col-md-12" }, [
           m("label", { for: "inputOxinoterapia" }, "Oxinoterapia"),
           m("div", { class: "input-group" }, [
-            m("select", { class: "custom-select" }, [
-              m("option", "Seleccione..."),
-              m(
-                "option",
-                { value: "1" },
-                "Fracción inspirada de oxígeno (FiO2)%"
-              ),
-              m("option", { value: "2" }, "Alto flujo (litro por minuto)"),
-              m("option", { value: "3" }, "Tienda facial"),
-              m("option", { value: "4" }, "Tubo en T"),
-            ]),
+            m(
+              "select",
+              {
+                class: "custom-select",
+                disabled: terapiaRespiratoriaController.habilitarCampos,
+              },
+              [
+                m("option", "Seleccione..."),
+                m(
+                  "option",
+                  { value: "1" },
+                  "Fracción inspirada de oxígeno (FiO2)%"
+                ),
+                m("option", { value: "2" }, "Alto flujo (litro por minuto)"),
+                m("option", { value: "3" }, "Tienda facial"),
+                m("option", { value: "4" }, "Tubo en T"),
+              ]
+            ),
             m("input", {
               class: "form-control",
               type: "number",
@@ -296,12 +342,19 @@ const FormularioDeRegistro = {
         m("div", { class: "form-group col-md-12" }, [
           m("label", { for: "inputMonitoreoPrevio" }, "Monitoreo Previo"),
           m("div", { class: "input-group" }, [
-            m("select", { class: "custom-select" }, [
-              m("option", "Seleccione..."),
-              m("option", { value: "1" }, "Saturación O2(%)"),
-              m("option", { value: "2" }, "Ventilación mecánica"),
-              m("option", { value: "3" }, "Ventilación no invasiva"),
-            ]),
+            m(
+              "select",
+              {
+                class: "custom-select",
+                disabled: terapiaRespiratoriaController.habilitarCampos,
+              },
+              [
+                m("option", "Seleccione..."),
+                m("option", { value: "1" }, "Saturación O2(%)"),
+                m("option", { value: "2" }, "Ventilación mecánica"),
+                m("option", { value: "3" }, "Ventilación no invasiva"),
+              ]
+            ),
             m("input", {
               class: "form-control",
               type: "number",
@@ -355,6 +408,7 @@ const FormularioDeRegistro = {
                     inputSuccion2.value = "";
                   }
                 },
+                disabled: terapiaRespiratoriaController.habilitarCampos,
               },
               [
                 m("option", "Seleccione..."),
@@ -385,6 +439,7 @@ const FormularioDeRegistro = {
               type: "radio",
               name: "customRadio",
               checked: "checked",
+              disabled: terapiaRespiratoriaController.habilitarCampos,
             }),
             m("label", { class: "custom-control-label" }, "Esputo"),
           ]),
@@ -408,6 +463,7 @@ const FormularioDeRegistro = {
           class: "form-control",
           id: "textAreaObservacionClinica",
           rows: "3",
+          disabled: terapiaRespiratoriaController.habilitarCampos,
         }),
       ]),
       m("div", { class: "form-group" }, [
@@ -420,10 +476,27 @@ const FormularioDeRegistro = {
           class: "form-control",
           id: "textAreaCriterio",
           rows: "3",
+          disabled: terapiaRespiratoriaController.habilitarCampos,
         }),
       ]),
-      m("button", { class: "btn btn-primary", type: "submit" }, "Guardar"),
-      m("button", { class: "btn btn-primary", type: "submit" }, "Eliminar"),
+      m(
+        "button",
+        {
+          class: "btn btn-primary",
+          type: "submit",
+          disabled: terapiaRespiratoriaController.habilitarCampos,
+        },
+        "Guardar"
+      ),
+      m(
+        "button",
+        {
+          class: "btn btn-primary",
+          type: "submit",
+          disabled: terapiaRespiratoriaController.habilitarCampos,
+        },
+        "Eliminar"
+      ),
     ]);
   },
 };
